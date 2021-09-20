9ffc114c421102c84c259f4c6e7e458f79b17ece
Tiago Moreno <santiago.moreno@astropay.com>
2021-09-20 18:28:58 -0500

    Modifiers

--- app/src/main/java/tech/yeswecode/hellocompose/MainActivity.kt
+++ app/src/main/java/tech/yeswecode/hellocompose/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tech.yeswecode.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,15 +29,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloCompose() {
-    Column {
-        Image(painterResource(id = R.drawable.platzi),
-            contentDescription = null)
-        Text("Este curso es lo mejor!!")
-        Text("Curso de Jetpack compose")
-        Button(onClick = {
+    Card(
+        elevation = 4.dp,
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painterResource(id = R.drawable.platzi),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp, 40.dp)
+            )
+            Text("Este curso es lo mejor!!",
+                style = MaterialTheme.typography.h4)
+            Text("Curso de Jetpack compose")
+            Button(onClick = {
 
-        }) {
-            Text("Click me")
+            }, modifier = Modifier.padding(top = 16.dp)) {
+                Text("Click me")
+            }
         }
     }
 }

e0f144060dd2c31a1e565f6b38cd5275d728ee6d
Tiago Moreno <santiago.moreno@astropay.com>
2021-09-20 17:18:51 -0500

    Hello compose

---
+++ app/src/main/java/tech/yeswecode/hellocompose/MainActivity.kt
@@ -0,0 +1,20 @@
+package tech.yeswecode.hellocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import tech.yeswecode.hellocompose.ui.theme.HelloComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hola Compose! :)")
+        }
+    }
+}

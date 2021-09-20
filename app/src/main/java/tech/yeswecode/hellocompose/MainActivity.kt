20e4a7c07cfe18638c9188bea2f7d646be1f612b
Tiago Moreno <santiago.moreno@astropay.com>
2021-09-20 17:55:55 -0500

    Componentes

--- app/src/main/java/tech/yeswecode/hellocompose/MainActivity.kt
+++ app/src/main/java/tech/yeswecode/hellocompose/MainActivity.kt
@@ -3,10 +3,15 @@ package tech.yeswecode.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import tech.yeswecode.hellocompose.ui.theme.HelloComposeTheme
 
@@ -21,7 +26,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloCompose() {
-    Text("Hello Compose :(")
+    Column {
+        Image(painterResource(id = R.drawable.platzi),
+            contentDescription = null)
+        Text("Este curso es lo mejor!!")
+        Text("Curso de Jetpack compose")
+        Button(onClick = {
+
+        }) {
+            Text("Click me")
+        }
+    }
 }
 
 @Preview(
